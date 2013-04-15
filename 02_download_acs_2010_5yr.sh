88c7496596f61432303af48f3c16ddaf509e8cc2
Ian Dees <ian.dees@gmail.com>
2013-04-15 09:24:13 -0500

    Fixes for download URLs.

--- 02_download_acs_2010_5yr.sh
+++ 02_download_acs_2010_5yr.sh
@@ -8,12 +8,12 @@ sudo chown ubuntu /mnt/tmp
 cd /mnt/tmp
 mkdir -p acs2010_5yr
 cd acs2010_5yr
-curl -O "http://www2.census.gov/acs2010_5yr/summaryfile/2007-2010_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/All_Geographies_Not_Tracts_Block_Groups.tar.gz"
-tar -xvzf All_Geographies_Not_Tracts_Block_Groups.tar.gz
-curl -O "http://www2.census.gov/acs2010_5yr/summaryfile/2007-2010_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/Tracts_Block_Groups_Only.tar.gz"
-tar -xvzf Tracts_Block_Groups_Only.tar.gz
-curl -O "http://www2.census.gov/acs2010_5yr/summaryfile/2007-2010_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/2010_ACS_Geography_Files.zip"
 sudo apt-get -y install unzip
+curl -O "http://www2.census.gov/acs2010_5yr/summaryfile/2006-2010_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/All_Geographies_Not_Tracts_Block_Groups.zip"
+unzip All_Geographies_Not_Tracts_Block_Groups.zip
+curl -O "http://www2.census.gov/acs2010_5yr/summaryfile/2006-2010_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/Tracts_Block_Groups_Only.zip"
+unzip Tracts_Block_Groups_Only.tar.gz
+curl -O "http://www2.census.gov/acs2010_5yr/summaryfile/2006-2010_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/2010_ACS_Geography_Files.zip"
 unzip 2010_ACS_Geography_Files.zip
 
 # Let the Postgres user access this data
