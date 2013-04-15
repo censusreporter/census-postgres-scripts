8cf5df1d4af455c823a0386baf6a1d449aedebf0
Ian Dees <ian.dees@gmail.com>
2013-04-15 10:18:18 -0500

    Unzipping 2010 puts the files inside a deep dir structure.

--- 02_download_acs_2010_5yr.sh
+++ 02_download_acs_2010_5yr.sh
@@ -17,5 +17,5 @@ curl -O "http://www2.census.gov/acs2010_5yr/summaryfile/2006-2010_ACSSF_All_In_2
 unzip 2010_ACS_Geography_Files.zip
 
 # Let the Postgres user access this data
-chmod 777 /mnt/tmp/acs2010_5yr/geog /mnt/tmp/acs2010_5yr/group1 /mnt/tmp/acs2010_5yr/group2
-chmod 644 /mnt/tmp/acs2010_5yr/geog/* /mnt/tmp/acs2010_5yr/group1/* /mnt/tmp/acs2010_5yr/group2/*
+chmod 777 /mnt/tmp/acs2010_5yr/geog /mnt/tmp/acs2010_5yr/tab4/sumfile/prod/2006thru2010/group1 /mnt/tmp/acs2010_5yr/tab4/sumfile/prod/2006thru2010/group2
+chmod 644 /mnt/tmp/acs2010_5yr/geog/* /mnt/tmp/acs2010_5yr/tab4/sumfile/prod/2006thru2010/group1/* /mnt/tmp/acs2010_5yr/tab4/sumfile/prod/2006thru2010/group2/*
