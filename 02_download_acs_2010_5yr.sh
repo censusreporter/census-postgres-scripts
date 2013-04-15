77a7eb3e4d09d17c52871b2e9a0f1aefbf04c2f7
Ian Dees <ian.dees@gmail.com>
2013-04-15 09:09:39 -0500

    Add ACS 2010 5yr version of the steps.

---
+++ 02_download_acs_2010_5yr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Make a data directory in ephemeral stoage
+sudo mkdir /mnt/tmp
+sudo chown ubuntu /mnt/tmp
+
+# Grab the 2010 ACS 5 year
+cd /mnt/tmp
+mkdir acs2010_5yr
+cd acs2010_5yr
+curl -O "http://www2.census.gov/acs2010_5yr/summaryfile/2007-2010_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/All_Geographies_Not_Tracts_Block_Groups.tar.gz"
+tar -xvzf All_Geographies_Not_Tracts_Block_Groups.tar.gz
+curl -O "http://www2.census.gov/acs2010_5yr/summaryfile/2007-2010_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/Tracts_Block_Groups_Only.tar.gz"
+tar -xvzf Tracts_Block_Groups_Only.tar.gz
+curl -O "http://www2.census.gov/acs2010_5yr/summaryfile/2007-2010_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/2010_ACS_Geography_Files.zip"
+sudo apt-get -y install unzip
+unzip 2010_ACS_Geography_Files.zip
+
+# Let the Postgres user access this data
+chmod 777 /mnt/tmp/acs2010_5yr/geog /mnt/tmp/acs2010_5yr/group1 /mnt/tmp/acs2010_5yr/group2
+chmod 644 /mnt/tmp/acs2010_5yr/geog/* /mnt/tmp/acs2010_5yr/group1/* /mnt/tmp/acs2010_5yr/group2/*
---
+++ 03_import_acs_2010_5yr.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Clone the import scripts from git
+cd /home/ubuntu
+sudo apt-get -y install git
+git clone git://github.com/censusreporter/census-postgres.git
+
+# Create the schema
+cd /home/ubuntu/census-postgres/acs2010_5yr
+sed -i '1i SET search_path = acs2010_5yr, public;' *.sql
+sudo -u postgres psql -c "DROP SCHEMA IF EXISTS acs2010_5yr; CREATE SCHEMA acs2010_5yr;"
+
+# Create import tables
+sudo -u postgres psql -f create_geoheader.sql
+sudo -u postgres psql -f geoheader_comments.sql
+sudo -u postgres psql -f create_tmp_geoheader.sql
+sudo -u postgres psql -f create_import_tables.sql
+
+# Correct the directories in the import scripts
+sed -i "s/\/<census_upload_root>\/acs2010_5yr\/All_Geographies_Not_Tracts_Block_Groups/\/mnt\/tmp\/acs2010_5yr\/geog/" /home/ubuntu/census-postgres/acs2010_5yr/import_geoheader.sql
+sed -i "s/\/<census_upload_root>\/acs2010_5yr\/All_Geographies_Not_Tracts_Block_Groups/\/mnt\/tmp\/acs2010_5yr\/group1/" /home/ubuntu/census-postgres/acs2010_5yr/import_sequences.sql
+sed -i "s/\/<census_upload_root>\/acs2010_5yr\/Tracts_Block_Groups_Only/\/mnt\/tmp\/acs2010_5yr\/group2/" /home/ubuntu/census-postgres/acs2010_5yr/import_sequences.sql
+
+# Slurp in the actual data
+sudo -u postgres psql -f import_geoheader.sql
+sudo -u postgres psql -f import_sequences.sql # This takes a while
+sudo -u postgres psql -f parse_tmp_geoheader.sql
+sudo -u postgres psql -f store_by_tables.sql
+sudo -u postgres psql -f insert_into_tables.sql # This takes ~8 hours
+sudo -u postgres psql -f view_estimate_stored_by_tables.sql
+sudo -u postgres psql -f view_moe_stored_by_tables.sql
+
+# Drop temp tables
+cd /home/ubuntu
+sudo -u postgres psql -f 04_drop_acs_2010_5yr_tmp_tables.sql
