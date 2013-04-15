ce080e09d53bb5b075eee2b5adab82f11dc12950
Ian Dees <ian.dees@gmail.com>
2013-04-15 10:06:40 -0500

    Just kidding. The file extension on Census' site is wrong.

--- 02_download_acs_2010_5yr.sh
+++ 02_download_acs_2010_5yr.sh
@@ -10,9 +10,9 @@ mkdir -p acs2010_5yr
 cd acs2010_5yr
 sudo apt-get -y install unzip
 curl -O "http://www2.census.gov/acs2010_5yr/summaryfile/2006-2010_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/All_Geographies_Not_Tracts_Block_Groups.zip"
-unzip All_Geographies_Not_Tracts_Block_Groups.zip
+tar -zxvf All_Geographies_Not_Tracts_Block_Groups.zip
 curl -O "http://www2.census.gov/acs2010_5yr/summaryfile/2006-2010_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/Tracts_Block_Groups_Only.zip"
-unzip Tracts_Block_Groups_Only.zip
+tar -zxvf Tracts_Block_Groups_Only.zip
 curl -O "http://www2.census.gov/acs2010_5yr/summaryfile/2006-2010_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/2010_ACS_Geography_Files.zip"
 unzip 2010_ACS_Geography_Files.zip
 
