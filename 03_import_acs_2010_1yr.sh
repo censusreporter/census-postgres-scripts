8d8eb2b7b7e92bbc146618a5c061dd3d170c241a
Ian Dees <ian.dees@gmail.com>
2013-04-25 17:55:49 -0500

    Removing some files that aren't created anymore.

--- 03_import_acs_2010_1yr.sh
+++ 03_import_acs_2010_1yr.sh
@@ -16,8 +16,6 @@ sudo -u postgres psql -f geoheader_comments.sql
 sudo -u postgres psql -f create_tmp_geoheader.sql
 sudo -u postgres psql -f drop_import_tables.sql
 sudo -u postgres psql -f create_import_tables.sql
-sudo -u postgres psql -f drop_import_moe.sql
-sudo -u postgres psql -f create_import_moe.sql
 
 # Correct the directories in the import scripts
 sed -i "s/\/<census_upload_root>\/acs2010_1yr\/All_Geographies_Not_Tracts_Block_Groups/\/mnt\/tmp\/acs2010_1yr\/geog/" /home/ubuntu/census-postgres/acs2010_1yr/import_geoheader.sql
@@ -26,13 +24,11 @@ sed -i "s/\/<census_upload_root>\/acs2010_1yr\/Tracts_Block_Groups_Only/\/mnt\/t
 
 # Slurp in the actual data
 sudo -u postgres psql -f import_geoheader.sql
-sudo -u postgres psql -f import_sequences.sql # This takes a while
+sudo -u postgres psql -f import_sequences.sql # This takes ~5 minutes
 sudo -u postgres psql -f parse_tmp_geoheader.sql
 sudo -u postgres psql -f store_by_tables.sql
-sudo -u postgres psql -f store_moe_by_tables.sql
-sudo -u postgres psql -f insert_into_tables.sql # This takes ~8 hours
-sudo -u postgres psql -f view_estimate_stored_by_tables.sql
-sudo -u postgres psql -f view_moe_stored_by_tables.sql
+sudo -u postgres psql -f insert_into_tables.sql # This takes ~5 minutes
+sudo -u postgres psql -f view_stored_by_tables.sql
 
 # Drop temp tables
 cd /home/ubuntu/census-postgres-scripts
