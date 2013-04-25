57488d53a68afa19e85a13d2a371e6476b64abc8
Ian Dees <ian.dees@gmail.com>
2013-04-25 16:38:56 -0500

    Create 03_import_acs_2010_1yr.sh

---
+++ 03_import_acs_2010_1yr.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Clone the import scripts from git
+cd /home/ubuntu
+sudo apt-get -y install git
+git clone git://github.com/censusreporter/census-postgres.git
+
+# Create the schema
+cd /home/ubuntu/census-postgres/acs2010_1yr
+sed -i '1i SET search_path = acs2010_1yr, public;' *.sql
+sudo -u postgres psql -c "DROP SCHEMA IF EXISTS acs2010_1yr; CREATE SCHEMA acs2010_1yr;"
+
+# Create import tables
+sudo -u postgres psql -f create_geoheader.sql
+sudo -u postgres psql -f geoheader_comments.sql
+sudo -u postgres psql -f create_tmp_geoheader.sql
+sudo -u postgres psql -f drop_import_tables.sql
+sudo -u postgres psql -f create_import_tables.sql
+sudo -u postgres psql -f drop_import_moe.sql
+sudo -u postgres psql -f create_import_moe.sql
+
+# Correct the directories in the import scripts
+sed -i "s/\/<census_upload_root>\/acs2010_1yr\/All_Geographies_Not_Tracts_Block_Groups/\/mnt\/tmp\/acs2010_1yr\/geog/" /home/ubuntu/census-postgres/acs2010_1yr/import_geoheader.sql
+sed -i "s/\/<census_upload_root>\/acs2010_1yr\/All_Geographies_Not_Tracts_Block_Groups/\/mnt\/tmp\/acs2010_1yr\/group1/" /home/ubuntu/census-postgres/acs2010_1yr/import_sequences.sql
+sed -i "s/\/<census_upload_root>\/acs2010_1yr\/Tracts_Block_Groups_Only/\/mnt\/tmp\/acs2010_1yr\/group2/" /home/ubuntu/census-postgres/acs2010_1yr/import_sequences.sql
+
+# Slurp in the actual data
+sudo -u postgres psql -f import_geoheader.sql
+sudo -u postgres psql -f import_sequences.sql # This takes a while
+sudo -u postgres psql -f parse_tmp_geoheader.sql
+sudo -u postgres psql -f store_by_tables.sql
+sudo -u postgres psql -f store_moe_by_tables.sql
+sudo -u postgres psql -f insert_into_tables.sql # This takes ~8 hours
+sudo -u postgres psql -f view_estimate_stored_by_tables.sql
+sudo -u postgres psql -f view_moe_stored_by_tables.sql
+
+# Drop temp tables
+cd /home/ubuntu/census-postgres-scripts
+sudo -u postgres psql -f 04_drop_acs_2010_1yr_tmp_tables.sql
