efbdad64cd067e7a6f3f0816dd1d0e671ddb60a3
Ian Dees <ian.dees@gmail.com>
2013-04-26 12:56:05 -0500

    Fix the 2009 1 and 3 year download scripts.

--- 02_download_acs_2009_1yr.sh
+++ 02_download_acs_2009_1yr.sh
@@ -9,6 +9,7 @@ cd /mnt/tmp
 mkdir -p acs2009_1yr
 cd acs2009_1yr
 sudo apt-get -y install unzip
-curl -O "http://www2.census.gov/acs2009_1yr/summaryfile/2009_ACSSF_All_In_1_Giant_File(Experienced-Users-Only)/All_Geographies.zip"
-unzip All_Geographies.zip
-curl -O "http://www2.census.gov/acs2009_1yr/summaryfile/Sequence_Number_and_Table_Number_Lookup.txt"
+curl -O "http://www2.census.gov/acs2009_1yr/summaryfile/Entire_SF/20091YRSF.zip"
+unzip 20091YRSF.zip
+curl -O "http://www2.census.gov/acs2009_1yr/summaryfile/UserTools/merge_5_6.txt"
+mv merge_5_6.txt Sequence_Number_and_Table_Number_Lookup.txt
