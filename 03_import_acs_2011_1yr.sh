ba8ebead2ae0bdb5722d594259a8cb9294df4edf
Ian Dees <ian.dees@gmail.com>
2013-04-25 21:48:22 -0500

    Add 2011 1yr import script and fix drops for 2010.

--- 03_import_acs_2011_1yr.sh
+++ 03_import_acs_2011_1yr.sh
@@ -6,9 +6,9 @@ sudo apt-get -y install git
 git clone git://github.com/censusreporter/census-postgres.git
 
 # Create the schema
-cd /home/ubuntu/census-postgres/acs2010_1yr
-sed -i '1i SET search_path = acs2010_1yr, public;' *.sql
-sudo -u postgres psql -c "DROP SCHEMA IF EXISTS acs2010_1yr; CREATE SCHEMA acs2010_1yr;"
+cd /home/ubuntu/census-postgres/acs2011_1yr
+sed -i '1i SET search_path = acs2011_1yr, public;' *.sql
+sudo -u postgres psql -c "DROP SCHEMA IF EXISTS acs2011_1yr; CREATE SCHEMA acs2011_1yr;"
 
 # Create import tables
 sudo -u postgres psql -f create_geoheader.sql
@@ -18,9 +18,9 @@ sudo -u postgres psql -f drop_import_tables.sql
 sudo -u postgres psql -f create_import_tables.sql
 
 # Correct the directories in the import scripts
-sed -i "s/\/<census_upload_root>\/acs2010_1yr\/All_Geographies_Not_Tracts_Block_Groups/\/mnt\/tmp\/acs2010_1yr\/geog/" /home/ubuntu/census-postgres/acs2010_1yr/import_geoheader.sql
-sed -i "s/\/<census_upload_root>\/acs2010_1yr\/All_Geographies_Not_Tracts_Block_Groups/\/mnt\/tmp\/acs2010_1yr\/group1/" /home/ubuntu/census-postgres/acs2010_1yr/import_sequences.sql
-sed -i "s/\/<census_upload_root>\/acs2010_1yr\/Tracts_Block_Groups_Only/\/mnt\/tmp\/acs2010_1yr\/group2/" /home/ubuntu/census-postgres/acs2010_1yr/import_sequences.sql
+sed -i "s/\/<census_upload_root>\/acs2011_1yr\/All_Geographies_Not_Tracts_Block_Groups/\/mnt\/tmp\/acs2011_1yr\/geog/" /home/ubuntu/census-postgres/acs2011_1yr/import_geoheader.sql
+sed -i "s/\/<census_upload_root>\/acs2011_1yr\/All_Geographies_Not_Tracts_Block_Groups/\/mnt\/tmp\/acs2011_1yr\/group1/" /home/ubuntu/census-postgres/acs2011_1yr/import_sequences.sql
+sed -i "s/\/<census_upload_root>\/acs2011_1yr\/Tracts_Block_Groups_Only/\/mnt\/tmp\/acs2011_1yr\/group2/" /home/ubuntu/census-postgres/acs2011_1yr/import_sequences.sql
 
 # Slurp in the actual data
 sudo -u postgres psql -f import_geoheader.sql
@@ -31,4 +31,4 @@ sudo -u postgres psql -f insert_into_tables.sql # This takes ~5 minutes
 sudo -u postgres psql -f view_stored_by_tables.sql
 
 # Drop temp tables
-sudo -u postgres psql -f 04_drop_import_tables.sql
+sudo -u postgres psql -f drop_import_tables.sql
