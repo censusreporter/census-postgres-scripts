2fb5d35911ef63569cd04cfbba91230a8c65bb06
Ian Dees <ian.dees@gmail.com>
2013-04-26 14:00:50 -0500

    Add ACS 2009 import scripts.

---
+++ 03_import_acs_2009_3yr.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Clone the import scripts from git
+cd /home/ubuntu
+sudo apt-get -y install git
+git clone git://github.com/censusreporter/census-postgres.git
+
+# Create the schema
+cd /home/ubuntu/census-postgres/acs2009_3yr
+sed -i '1i SET search_path = acs2009_3yr, public;' *.sql
+sudo -u postgres psql -c "DROP SCHEMA IF EXISTS acs2009_3yr; CREATE SCHEMA acs2009_3yr;"
+
+# Create import tables
+sudo -u postgres psql -f create_geoheader.sql
+sudo -u postgres psql -f geoheader_comments.sql
+sudo -u postgres psql -f create_tmp_geoheader.sql
+sudo -u postgres psql -f drop_import_tables.sql
+sudo -u postgres psql -f create_import_tables.sql
+
+# Correct the directories in the import scripts
+sed -i "s/\/<census_upload_root>\/acs2009_3yr\/All_Geographies_Not_Tracts_Block_Groups/\/mnt\/tmp\/acs2009_3yr\/geog/" /home/ubuntu/census-postgres/acs2009_3yr/import_geoheader.sql
+sed -i "s/\/<census_upload_root>\/acs2009_3yr\/All_Geographies_Not_Tracts_Block_Groups/\/mnt\/tmp\/acs2009_3yr\/group1/" /home/ubuntu/census-postgres/acs2009_3yr/import_sequences.sql
+sed -i "s/\/<census_upload_root>\/acs2009_3yr\/Tracts_Block_Groups_Only/\/mnt\/tmp\/acs2009_3yr\/group2/" /home/ubuntu/census-postgres/acs2009_3yr/import_sequences.sql
+
+# Slurp in the actual data
+sudo -u postgres psql -f import_geoheader.sql
+sudo -u postgres psql -f import_sequences.sql # This takes ~5 minutes
+sudo -u postgres psql -f parse_tmp_geoheader.sql
+sudo -u postgres psql -f store_by_tables.sql
+sudo -u postgres psql -f insert_into_tables.sql # This takes ~5 minutes
+sudo -u postgres psql -f view_stored_by_tables.sql
+
+# Drop temp tables
+sudo -u postgres psql -f drop_import_tables.sql
