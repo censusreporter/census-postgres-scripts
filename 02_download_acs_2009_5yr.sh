2c05f1cae2334c48ab4e44e9fa1a596f675cb01d
Ian Dees <ian.dees@gmail.com>
2013-04-26 12:45:32 -0500

    Fix unzipping for the 2009 5yr.

--- 02_download_acs_2009_5yr.sh
+++ 02_download_acs_2009_5yr.sh
@@ -6,21 +6,19 @@ sudo chown ubuntu /mnt/tmp
 
 # Grab the 2009 ACS 5 year
 cd /mnt/tmp
-mkdir -p acs2009_5yr
-cd acs2009_5yr
+mkdir -p acs2009_5yr/group{1,2}
+cd acs2009_5yr/group1
 sudo apt-get -y install unzip
 curl -O "http://www2.census.gov/acs2009_5yr/summaryfile/2005-2009_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/All_Geographies_Not_Tracts_Block_Groups.zip"
-tar -zxvf All_Geographies_Not_Tracts_Block_Groups.zip
+unzip All_Geographies_Not_Tracts_Block_Groups.zip
+# This zip contains per-state zips, so unzip those too
+for i in *_All_Geographies_Not_Tracts_Block_Groups.zip; do unzip $i; done
+cd ../group2
 curl -O "http://www2.census.gov/acs2009_5yr/summaryfile/2005-2009_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/Tracts_Block_Groups_Only.zip"
-tar -zxvf Tracts_Block_Groups_Only.zip
-curl -O "http://www2.census.gov/acs2009_5yr/summaryfile/2005-2009_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/2009_ACS_Geography_Files.zip"
-unzip 2009_ACS_Geography_Files.zip
+unzip Tracts_Block_Groups_Only.zip
+cd ..
 curl -O "http://www2.census.gov/acs2009_5yr/summaryfile/UserTools/Sequence_Number_and_Table_Number_Lookup.txt"
 
-# The 2009 ACS unzips into deep directory structure,
-# so pull it into something sane
-mv tab4/sumfile/prod/2005thru2009/group* .
-
 # Let the Postgres user access this data
-chmod 777 /mnt/tmp/acs2009_5yr/geog /mnt/tmp/acs2009_5yr/group1 /mnt/tmp/acs2009_5yr/group2
-chmod 644 /mnt/tmp/acs2009_5yr/geog/* /mnt/tmp/acs2009_5yr/group1/* /mnt/tmp/acs2009_5yr/group2/*
+chmod 777 /mnt/tmp/acs2009_5yr/group1 /mnt/tmp/acs2009_5yr/group2
+chmod 644 /mnt/tmp/acs2009_5yr/group1/* /mnt/tmp/acs2009_5yr/group2/*
