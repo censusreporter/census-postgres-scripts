ffbddd1706f19c2780b85e246b3af99476aa2f9c
Ian Dees <ian.dees@gmail.com>
2013-05-21 07:06:05 -0500

    Typo in the path of the 2008 3yr.

--- 02_download_acs_2008_3yr.sh
+++ 02_download_acs_2008_3yr.sh
@@ -13,7 +13,7 @@ curl -O "http://www2.census.gov/acs2008_3yr/summaryfile/all_ACSSF.zip"
 unzip all_ACSSF.zip
 
 rm all_ACSSF.zip
-for i in prt03/prod/sumfile/data/2008/**/20083*.zip; do unzip -n $i; done
+for i in prt03/prod/sumfile/data/2006thru2008/**/20083*.zip; do unzip -n $i; done
 
 # The lookup tables are in XLS only, so they'll be provided in the census-postgres
 # package later.
