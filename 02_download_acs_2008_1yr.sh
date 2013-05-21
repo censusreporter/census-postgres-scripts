3f32c774ce004e71363679ec39c6022749a67938
Ian Dees <ian.dees@gmail.com>
2013-05-20 21:08:24 -0500

    Unzip the downloaded 2008 files correctly.

--- 02_download_acs_2008_1yr.sh
+++ 02_download_acs_2008_1yr.sh
@@ -11,5 +11,9 @@ cd acs2008_1yr
 sudo apt-get -y install unzip
 curl -O "http://www2.census.gov/acs2008_1yr/summaryfile/all_ACSSF.zip"
 unzip all_ACSSF.zip
-curl -O "http://www2.census.gov/acs2008_1yr/summaryfile/merge_5_6.xls"
-mv merge_5_6.xls Sequence_Number_and_Table_Number_Lookup.txt
+
+rm all_ACSSF.zip
+for i in prt03/prod/sumfile/data/2008/**/20081*.zip; do unzip -n $i; done
+
+# The lookup tables are in XLS only, so they'll be provided in the census-postgres
+# package later.
