3cdc560ec61a13471efd75f4d601aefe0b46f456
Ian Dees <ian.dees@gmail.com>
2013-06-12 15:14:30 -0500

    Delete the sql along the way.

--- 13_import_tiger_2012.sh
+++ 13_import_tiger_2012.sh
@@ -7,7 +7,7 @@ sudo -u postgres psql -c "DROP SCHEMA IF EXISTS tiger2012; CREATE SCHEMA tiger20
 for i in AIANNH AITS ANRC BG CBSA CD COUNTY COUSUB CSA ELSD PLACE PUMA SCSD SLDL SLDU STATE ZCTA5
 do
     # Start by preparing the table
-    shp2pgsql -W "latin1" -s 4326 -p `ls -a $i/*.shp | head -n 1` tiger2012.$i >> $i.sql
+    shp2pgsql -W "latin1" -s 4326 -p `ls -a $i/*.shp | head -n 1` tiger2012.$i > $i.sql
 
     # Then append all the geometries
     shp2pgsql -W "latin1" -s 4326 -a -I $i/*.shp tiger2012.$i >> $i.sql
@@ -20,4 +20,6 @@ do
         echo "Couldn't import $i.sql."
         exit 1
     fi
+
+    rm $i.sql
 done
