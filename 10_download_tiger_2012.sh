11312422984932944314981a85f9d947a3bf9640
Ian Dees <ian.dees@gmail.com>
2013-06-12 09:02:28 -0400

    Create 10_download_tiger_2012.sh

---
+++ 10_download_tiger_2012.sh
@@ -0,0 +1,18 @@
+# Download census-y TIGER data
+
+wget --mirror ftp://ftp2.census.gov/geo/tiger/TIGER2012/STATE/tl_2012_us_state.zip \
+              ftp://ftp2.census.gov/geo/tiger/TIGER2012/CBSA/tl_2012_us_cbsa.zip \
+              ftp://ftp2.census.gov/geo/tiger/TIGER2012/CD/tl_2012_us_cd112.zip \
+              ftp://ftp2.census.gov/geo/tiger/TIGER2012/CSA/tl_2012_us_csa.zip \
+              ftp://ftp2.census.gov/geo/tiger/TIGER2012/PLACE/*.zip \
+              ftp://ftp2.census.gov/geo/tiger/TIGER2012/ELSD/*.zip \
+              ftp://ftp2.census.gov/geo/tiger/TIGER2012/SCSD/*.zip \
+              ftp://ftp2.census.gov/geo/tiger/TIGER2012/ZCTA5/tl_2012_us_zcta510.zip \
+              ftp://ftp2.census.gov/geo/tiger/TIGER2012/COUSUB/*.zip \
+              ftp://ftp2.census.gov/geo/tiger/TIGER2012/PUMA/*.zip \
+              ftp://ftp2.census.gov/geo/tiger/TIGER2012/SLDL/*.zip \
+              ftp://ftp2.census.gov/geo/tiger/TIGER2012/SLDU/*.zip \
+              ftp://ftp2.census.gov/geo/tiger/TIGER2012/AIANNH/tl_2012_us_aiannh.zip \
+              ftp://ftp2.census.gov/geo/tiger/TIGER2012/AITS/tl_2012_us_aitsn.zip \
+              ftp://ftp2.census.gov/geo/tiger/TIGER2012/ANRC/tl_2012_02_anrc.zip \
+              ftp://ftp2.census.gov/geo/tiger/TIGER2012/BG/*.zip
