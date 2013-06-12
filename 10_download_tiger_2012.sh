36b9932b5bd857b1d24dff8a46089abbad77916d
Ian Dees <ian.dees@gmail.com>
2013-06-12 09:24:00 -0500

    Forgot to include county shapefiles.

--- 10_download_tiger_2012.sh
+++ 10_download_tiger_2012.sh
@@ -1,6 +1,7 @@
 # Download census-y TIGER data
 
-wget --mirror ftp://ftp2.census.gov/geo/tiger/TIGER2012/STATE/tl_2012_us_state.zip \
+wget --mirror ftp://ftp2.census.gov/geo/tiger/TIGER2012/COUNTY/tl_2012_us_county.zip \
+              ftp://ftp2.census.gov/geo/tiger/TIGER2012/STATE/tl_2012_us_state.zip \
               ftp://ftp2.census.gov/geo/tiger/TIGER2012/CBSA/tl_2012_us_cbsa.zip \
               ftp://ftp2.census.gov/geo/tiger/TIGER2012/CD/tl_2012_us_cd112.zip \
               ftp://ftp2.census.gov/geo/tiger/TIGER2012/CSA/tl_2012_us_csa.zip \
