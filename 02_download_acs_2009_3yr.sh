2329c64c94da03c6a957a2ab24e35f040b0e65aa
Ian Dees <ian.dees@gmail.com>
2013-09-16 21:52:05 -0500

    Fix the URL to 2009 3yr's sqn lookup

--- 02_download_acs_2009_3yr.sh
+++ 02_download_acs_2009_3yr.sh
@@ -11,4 +11,4 @@ cd acs2009_3yr
 sudo apt-get -y install unzip
 curl -O "http://www2.census.gov/acs2009_3yr/summaryfile/2007-2009_ACSSF_All_In_1_Giant_File(Experienced-Users-Only)/All_Geographies.zip"
 unzip All_Geographies.zip
-curl -O "http://www2.census.gov/acs2009_3yr/summaryfile/Sequence_Number_and_Table_Number_Lookup.xls"
+curl -O "http://www2.census.gov/acs2009_3yr/summaryfile/UserTools/Sequence_Number_and_Table_Number_Lookup.txt"
