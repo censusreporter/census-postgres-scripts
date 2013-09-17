fb1c007d6742f15fc419023c11e004524225ad3e
Ian Dees <ian.dees@gmail.com>
2013-09-16 21:59:03 -0500

    Adding the 2011 1yr sqn lookup txt

--- 02_download_acs_2011_1yr.sh
+++ 02_download_acs_2011_1yr.sh
@@ -11,3 +11,4 @@ cd acs2011_1yr
 sudo apt-get -y install unzip
 curl -O "http://www2.census.gov/acs2011_1yr/summaryfile/2011_ACSSF_All_In_1_Giant_File(Experienced-Users-Only)/All_Geographies.zip"
 unzip All_Geographies.zip
+curl -O "http://www2.census.gov/acs2011_1yr/summaryfile/Sequence_Number_and_Table_Number_Lookup.txt"
