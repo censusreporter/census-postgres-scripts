c050731283cd729b02357f24ed4061d6b444581f
Ian Dees <ian.dees@gmail.com>
2013-09-17 14:00:18 -0500

    Add status comments

--- 03_import_acs_2007_3yr.sh
+++ 03_import_acs_2007_3yr.sh
@@ -10,24 +10,28 @@ cd /home/ubuntu/census-postgres/acs2007_3yr
 sudo -u postgres psql -c "DROP SCHEMA IF EXISTS acs2007_3yr CASCADE; CREATE SCHEMA acs2007_3yr;"
 
 # Create import tables
+echo "Creating geoheader"
 sudo -u postgres psql -v ON_ERROR_STOP=1 -q -f create_geoheader.sql
 if [[ $? != 0 ]]; then
     echo "Failed creating geoheader."
     exit 1
 fi
 
+echo "Creating geoheader comments"
 sudo -u postgres psql -v ON_ERROR_STOP=1 -q -f geoheader_comments.sql
 if [[ $? != 0 ]]; then
     echo "Failed creating geoheader comments."
     exit 1
 fi
 
+echo "Creating temp geoheader"
 sudo -u postgres psql -v ON_ERROR_STOP=1 -q -f create_tmp_geoheader.sql
 if [[ $? != 0 ]]; then
     echo "Failed creating temp geoheader."
     exit 1
 fi
 
+echo "Creating temp import tables"
 sudo -u postgres psql -v ON_ERROR_STOP=1 -q -f create_import_tables.sql
 if [[ $? != 0 ]]; then
     echo "Failed creating temp import tables."
@@ -35,36 +39,42 @@ if [[ $? != 0 ]]; then
 fi
 
 # Slurp in the actual data
+echo "Importing geoheader"
 sudo -u postgres psql -v ON_ERROR_STOP=1 -q -f import_geoheader.sql
 if [[ $? != 0 ]]; then
     echo "Failed importing geoheader."
     exit 1
 fi
 
+echo "Importing sequences"
 sudo -u postgres psql -v ON_ERROR_STOP=1 -q -f import_sequences.sql # This takes ~5 minutes
 if [[ $? != 0 ]]; then
     echo "Failed importing sequences."
     exit 1
 fi
 
+echo "Parsing geoheader"
 sudo -u postgres psql -v ON_ERROR_STOP=1 -q -f parse_tmp_geoheader.sql
 if [[ $? != 0 ]]; then
     echo "Failed parsing geoheader."
     exit 1
 fi
 
+echo "Storing tables"
 sudo -u postgres psql -v ON_ERROR_STOP=1 -q -f store_by_tables.sql
 if [[ $? != 0 ]]; then
     echo "Failed storing tables."
     exit 1
 fi
 
+echo "Inserting into tables"
 sudo -u postgres psql -v ON_ERROR_STOP=1 -q -f insert_into_tables.sql # This takes ~5 minutes
 if [[ $? != 0 ]]; then
     echo "Failed inserting into tables."
     exit 1
 fi
 
+echo "Creating views"
 sudo -u postgres psql -v ON_ERROR_STOP=1 -q -f view_stored_by_tables.sql
 if [[ $? != 0 ]]; then
     echo "Failed creating views."
@@ -72,6 +82,7 @@ if [[ $? != 0 ]]; then
 fi
 
 # Drop temp tables
+echo "Dropping import tables"
 sudo -u postgres psql -v ON_ERROR_STOP=1 -q -f drop_import_tables.sql
 if [[ $? != 0 ]]; then
     echo "Failed dropping import tables."
