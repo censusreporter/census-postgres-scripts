656894a3023aa8528bb311b87126f98ecee60734
Ian Dees <ian.dees@gmail.com>
2013-09-27 12:24:57 -0500

    After expanding all this stuff, we don't need to keep it around.

--- 02_download_acs_2008_3yr.sh
+++ 02_download_acs_2008_3yr.sh
@@ -15,5 +15,7 @@ unzip all_ACSSF.zip
 rm all_ACSSF.zip
 for i in prt03/prod/sumfile/data/2006thru2008/**/20083*.zip; do unzip -n $i; done
 
+rm -rf prt03
+
 # The lookup tables are in XLS only, so they'll be provided in the census-postgres
 # package later.
