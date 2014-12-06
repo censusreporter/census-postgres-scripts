f0c86008210854de484779dc1a15d477b388357c
Ian Dees <ian.dees@gmail.com>
2014-12-06 11:01:09 -0500

    Don't preallocate disk space

--- 02_download_acs_2013_5yr.sh
+++ 02_download_acs_2013_5yr.sh
@@ -9,7 +9,7 @@ cd /mnt/tmp
 mkdir -p acs2013_5yr
 cd acs2013_5yr
 sudo apt-get -y install aria2 unzip
-aria2c --dir=/mnt/tmp/acs2013_5yr --max-connection-per-server=5 --force-sequential=true \
+aria2c --dir=/mnt/tmp/acs2013_5yr --max-connection-per-server=5 --force-sequential=true --file-allocation=none \
     "http://www2.census.gov/acs2013_5yr/summaryfile/2009-2013_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/All_Geographies_Not_Tracts_Block_Groups.tar.gz" \
     "http://www2.census.gov/acs2013_5yr/summaryfile/2009-2013_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/Tracts_Block_Groups_Only.tar.gz" \
     "http://www2.census.gov/acs2013_5yr/summaryfile/2009-2013_ACSSF_All_In_2_Giant_Files(Experienced-Users-Only)/2013_ACS_Geography_Files.zip" \
