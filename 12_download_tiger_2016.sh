a763a8d2f702899895e77dbb0b7a452936f9c84a
Ian Dees <ian.dees@gmail.com>
2018-01-02 21:06:41 -0600

    Sort the TIGER download types and remove blocks because ACS doesn't support it

--- 12_download_tiger_2016.sh
+++ 12_download_tiger_2016.sh
@@ -6,33 +6,32 @@ wget --recursive --continue --accept=*.zip \
      --no-parent --cut-dirs=3 --no-host-directories \
      -e robots=off \
      --directory-prefix=/mnt/tmp/tiger2016 \
-    https://www2.census.gov/geo/tiger/TIGER2016/COUNTY/ \
-    https://www2.census.gov/geo/tiger/TIGER2016/STATE/ \
-    https://www2.census.gov/geo/tiger/TIGER2016/CBSA/ \
-    https://www2.census.gov/geo/tiger/TIGER2016/CD/ \
-    https://www2.census.gov/geo/tiger/TIGER2016/CSA/ \
-    https://www2.census.gov/geo/tiger/TIGER2016/PLACE/ \
-    https://www2.census.gov/geo/tiger/TIGER2016/ELSD/ \
-    https://www2.census.gov/geo/tiger/TIGER2016/SCSD/ \
-    https://www2.census.gov/geo/tiger/TIGER2016/ZCTA5/ \
-    https://www2.census.gov/geo/tiger/TIGER2016/COUSUB/ \
-    https://www2.census.gov/geo/tiger/TIGER2016/PUMA/ \
-    https://www2.census.gov/geo/tiger/TIGER2016/SLDL/ \
-    https://www2.census.gov/geo/tiger/TIGER2016/SLDU/ \
     https://www2.census.gov/geo/tiger/TIGER2016/AIANNH/ \
     https://www2.census.gov/geo/tiger/TIGER2016/AITS/ \
     https://www2.census.gov/geo/tiger/TIGER2016/ANRC/ \
     https://www2.census.gov/geo/tiger/TIGER2016/BG/ \
+    https://www2.census.gov/geo/tiger/TIGER2016/CBSA/ \
+    https://www2.census.gov/geo/tiger/TIGER2016/CD/ \
     https://www2.census.gov/geo/tiger/TIGER2016/CNECTA/ \
     https://www2.census.gov/geo/tiger/TIGER2016/CONCITY/ \
+    https://www2.census.gov/geo/tiger/TIGER2016/COUNTY/ \
+    https://www2.census.gov/geo/tiger/TIGER2016/COUSUB/ \
+    https://www2.census.gov/geo/tiger/TIGER2016/CSA/ \
+    https://www2.census.gov/geo/tiger/TIGER2016/ELSD/ \
     https://www2.census.gov/geo/tiger/TIGER2016/METDIV/ \
     https://www2.census.gov/geo/tiger/TIGER2016/NECTA/ \
     https://www2.census.gov/geo/tiger/TIGER2016/NECTADIV/ \
+    https://www2.census.gov/geo/tiger/TIGER2016/PLACE/ \
+    https://www2.census.gov/geo/tiger/TIGER2016/PUMA/ \
+    https://www2.census.gov/geo/tiger/TIGER2016/SCSD/ \
+    https://www2.census.gov/geo/tiger/TIGER2016/SLDL/ \
+    https://www2.census.gov/geo/tiger/TIGER2016/SLDU/ \
+    https://www2.census.gov/geo/tiger/TIGER2016/STATE/ \
     https://www2.census.gov/geo/tiger/TIGER2016/SUBMCD/ \
     https://www2.census.gov/geo/tiger/TIGER2016/TBG/ \
-    https://www2.census.gov/geo/tiger/TIGER2016/TTRACT/ \
-    https://www2.census.gov/geo/tiger/TIGER2016/TABBLOCK/ \
     https://www2.census.gov/geo/tiger/TIGER2016/TRACT/ \
+    https://www2.census.gov/geo/tiger/TIGER2016/TTRACT/ \
     https://www2.census.gov/geo/tiger/TIGER2016/UAC/ \
     https://www2.census.gov/geo/tiger/TIGER2016/UNSD/ \
-    https://www2.census.gov/geo/tiger/TIGER2016/VTD/
+    https://www2.census.gov/geo/tiger/TIGER2016/VTD/ \
+    https://www2.census.gov/geo/tiger/TIGER2016/ZCTA5/
