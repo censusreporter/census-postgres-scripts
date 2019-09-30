2b5612b1ff8d98a0d1970170e41f7b91b2ccb81b
Ian Dees <ian.dees@gmail.com>
2019-09-30 06:32:53 -0500

    Adjust the download script for 2018 as aria2c seems to get blocked

--- 02_download_acs_2018_1yr.sh
+++ 02_download_acs_2018_1yr.sh
@@ -8,9 +8,7 @@ sudo chown ubuntu /mnt/tmp
 cd /mnt/tmp
 mkdir -p acs2018_1yr
 cd acs2018_1yr
-sudo apt-get -y install unzip aria2
-aria2c --dir=/mnt/tmp/acs2018_1yr --max-connection-per-server=5 --force-sequential=true \
-    "http://www2.census.gov/programs-surveys/acs/summary_file/2018/data/1_year_entire_sf/All_Geographies.zip" \
-    "http://www2.census.gov/programs-surveys/acs/summary_file/2018/documentation/user_tools/ACS_1yr_Seq_Table_Number_Lookup.txt"
+sudo apt-get -y install unzip
+curl -L "http://www2.census.gov/programs-surveys/acs/summary_file/2018/data/1_year_entire_sf/All_Geographies.zip" -o /mnt/tmp/acs2018_1yr/All_Geographies.zip
+curl -L "http://www2.census.gov/programs-surveys/acs/summary_file/2018/documentation/user_tools/ACS_1yr_Seq_Table_Number_Lookup.txt" -o /mnt/tmp/acs2018_1yr/Sequence_Number_and_Table_Number_Lookup.txt
 unzip -q All_Geographies.zip
-mv /mnt/tmp/acs2018_1yr/ACS_1yr_Seq_Table_Number_Lookup.txt /mnt/tmp/acs2018_1yr/Sequence_Number_and_Table_Number_Lookup.txt
