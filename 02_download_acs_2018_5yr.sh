484f8eddf23f7132c18816d270cdc143c9d90b99
Ian Dees <ian.dees@gmail.com>
2019-12-29 09:36:34 -0600

    Modify the download script to handle zipped zips

--- 02_download_acs_2018_5yr.sh
+++ 02_download_acs_2018_5yr.sh
@@ -20,6 +20,12 @@ curl -L "https://www2.census.gov/programs-surveys/acs/summary_file/2018/document
 
 unzip -q /mnt/tmp/acs2018_5yr/All_Geographies_Not_Tracts_Block_Groups.zip
 unzip -q /mnt/tmp/acs2018_5yr/Tracts_Block_Groups_Only.zip
+
+# The .zip's contains .zip's for each state, but they need to be placed in their own directory or the contents of the .zips will clobber each other
+mkdir -p tracts_block_groups_only all_geographies_not_tracts_block_groups
+for i in *_Tracts_Block_Groups_Only.zip; do unzip -q -d tracts_block_groups_only $i && rm $i; done
+for i in *_All_Geographies_Not_Tracts_Block_Groups.zip; do unzip -q -d all_geographies_not_tracts_block_groups $i && rm $i; done
+
 unzip -q /mnt/tmp/acs2018_5yr/2018_ACS_Geography_Files.zip
 
 mv ACS_5yr_Seq_Table_Number_Lookup.txt Sequence_Number_and_Table_Number_Lookup.txt
