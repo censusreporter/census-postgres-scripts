1981b7e6112ce271c26fc092399a3e25485b8c44
Joe Germuska <joe@germuska.com>
2021-07-12 12:29:12 -0500

    import 2010 blocks only

---
+++ 12_download_tiger_2010.sh
@@ -0,0 +1,11 @@
+# Download census-y TIGER data
+# newest files for 2010 blocks are in https://census-backup.b-cdn.net/geo/tiger/TIGER2020/TABBLOCK/
+# Includes "Island areas" blocks which aren't relevant to our project but we'll take 'em for now
+# 2020 are in https://census-backup.b-cdn.net/geo/tiger/TIGER2020/TABBLOCK20/
+mkdir -p /home/ubuntu/data/tiger2010
+
+wget --recursive --continue --accept=*.zip \
+     --no-parent --cut-dirs=3 --no-host-directories \
+     -e robots=off \
+     --directory-prefix=/home/ubuntu/data/tiger2010 \
+    https://census-backup.b-cdn.net/geo/tiger/TIGER2020/TABBLOCK/
