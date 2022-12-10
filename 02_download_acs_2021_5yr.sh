a7bbfbd4bea77244b2db5d285c852c86b2db445e
Ian Dees <ian.dees@gmail.com>
2022-12-09 21:47:19 -0600

    Add in the sequence-based-SF/ part of the URL

--- 02_download_acs_2021_5yr.sh
+++ 02_download_acs_2021_5yr.sh
@@ -5,13 +5,13 @@ cd /home/ubuntu/data/acs2021_5yr
 
 # Grab the 2021 ACS 5 year
 sudo apt-get -y install unzip curl
-curl -L "https://census-backup.b-cdn.net/programs-surveys/acs/summary_file/2021/data/5_year_entire_sf/All_Geographies_Not_Tracts_Block_Groups.zip" \
+curl -L "https://census-backup.b-cdn.net/programs-surveys/acs/summary_file/2021/sequence-based-SF/data/5_year_entire_sf/All_Geographies_Not_Tracts_Block_Groups.zip" \
      -o /home/ubuntu/data/acs2021_5yr/All_Geographies_Not_Tracts_Block_Groups.zip
-curl -L "https://census-backup.b-cdn.net/programs-surveys/acs/summary_file/2021/data/5_year_entire_sf/Tracts_Block_Groups_Only.zip" \
+curl -L "https://census-backup.b-cdn.net/programs-surveys/acs/summary_file/2021/sequence-based-SF/data/5_year_entire_sf/Tracts_Block_Groups_Only.zip" \
      -o /home/ubuntu/data/acs2021_5yr/Tracts_Block_Groups_Only.zip
-curl -L "https://census-backup.b-cdn.net/programs-surveys/acs/summary_file/2021/data/5_year_entire_sf/2021_ACS_Geography_Files.zip" \
+curl -L "https://census-backup.b-cdn.net/programs-surveys/acs/summary_file/2021/sequence-based-SF/data/5_year_entire_sf/2021_ACS_Geography_Files.zip" \
      -o /home/ubuntu/data/acs2021_5yr/2021_ACS_Geography_Files.zip
-curl -L "https://census-backup.b-cdn.net/programs-surveys/acs/summary_file/2021/documentation/user_tools/ACS_5yr_Seq_Table_Number_Lookup.txt" \
+curl -L "https://census-backup.b-cdn.net/programs-surveys/acs/summary_file/2021/sequence-based-SF/documentation/user_tools/ACS_5yr_Seq_Table_Number_Lookup.txt" \
      -o /home/ubuntu/data/acs2021_5yr/ACS_5yr_Seq_Table_Number_Lookup.txt
 
 unzip -q /home/ubuntu/data/acs2021_5yr/All_Geographies_Not_Tracts_Block_Groups.zip
