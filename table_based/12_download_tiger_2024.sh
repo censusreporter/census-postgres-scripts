a5b2302747e72e5b9cc163b8d51e2c1cced024aa
Ian Dees <ian.dees@gmail.com>
2026-02-07 07:29:07 -0600

    Switch to www2 for tiger2024

--- table_based/12_download_tiger_2024.sh
+++ table_based/12_download_tiger_2024.sh
@@ -6,27 +6,27 @@ wget --recursive --continue --accept=*.zip \
      --no-parent --cut-dirs=3 --no-host-directories \
      -e robots=off \
      --directory-prefix=/home/ubuntu/data/tiger2024 \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/AIANNH/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/AITSN/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/ANRC/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/BG/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/CBSA/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/CD/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/CONCITY/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/COUNTY/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/COUSUB/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/CSA/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/ELSD/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/METDIV/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/PLACE/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/PUMA/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/SCSD/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/SLDL/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/SLDU/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/STATE/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/TBG/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/TRACT/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/TTRACT/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/UAC/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/UNSD/ \
-    https://census-backup.b-cdn.net/geo/tiger/TIGER2024/ZCTA520/
+    https://www2.census.gov/geo/tiger/TIGER2024/AIANNH/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/AITSN/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/ANRC/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/BG/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/CBSA/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/CD/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/CONCITY/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/COUNTY/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/COUSUB/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/CSA/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/ELSD/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/METDIV/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/PLACE/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/PUMA/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/SCSD/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/SLDL/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/SLDU/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/STATE/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/TBG/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/TRACT/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/TTRACT/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/UAC/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/UNSD/ \
+    https://www2.census.gov/geo/tiger/TIGER2024/ZCTA520/
